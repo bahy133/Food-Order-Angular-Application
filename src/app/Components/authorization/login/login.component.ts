ebe4c14bf8455b8a1c4b832d07570bdad0de8a01
bahy133 <bahy713@gmail.com>
2022-11-05 18:12:18 +0200

    27th commit

--- src/app/Components/authorization/login/login.component.ts
+++ src/app/Components/authorization/login/login.component.ts
@@ -1,6 +1,10 @@
+import { User } from './../../../Models/user';
+import { UserService } from './../../../Services/user.service';
+import { Router } from '@angular/router';
 import { LanguagedirService } from './../../../Services/languagedir.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +12,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  userLogin!: FormGroup;
+  userdata!: User;
+  error: boolean = false;
   constructor(
     public translate: TranslateService,
-    public language: LanguagedirService
-  ) {}
-
+    public language: LanguagedirService,
+    private fb: FormBuilder,
+    private router: Router,
+    private userser: UserService
+  ) {
+    this.userLogin = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+  get email() {
+    return this.userLogin.get('email');
+  }
+  get pass() {
+    return this.userLogin.get('password');
+  }
+  Loginfn() {
+    console.log('in login');
+    this.userser.getUser(this.email?.value).subscribe((data) => {
+      this.userdata = data;
+    });
+    if (this.userdata) {
+      if (this.pass?.value == this.userdata.password) {
+        localStorage.setItem('token', this.userdata.email);
+        this.router.navigate(['/Home']);
+        this.error = false;
+      } else {
+        this.error = true;
+      }
+    }
+    this.error = true;
+  }
   ngOnInit(): void {}
 }
